87b3640458df266cd2fa049547de2057bc71f721
vagrant <vagrant@precise32.(none)>
2014-03-12 02:13:47 +0000

    set reminder edit link, history button, water image on login

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
 $('#aboutLink').on('click', function() {
   ga('send', 'event', 'about', 'click');
 });
+
+$(document).ready(function() {
+	$width = $('#imgcontent').width();
+	
+	$('#imgcontent img').css({
+		'max-width': $width, 'height': 'auto', 'align': 'middle'
+	});
+});
